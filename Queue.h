aafc850c41516dcdb5f907d3f6f78c235537a028
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-02 20:43:16 +0300

    v 4.6

--- Queue.h
+++ Queue.h
@@ -11,8 +11,19 @@ public:
     T m_data;
     Node* m_next;
 
+    /*
+     * constructor for class Node
+     */
     explicit Node(const T data): m_data(data), m_next(nullptr){};
+
+    /*
+     * copy constructor for class Node
+     */
     Node(const Node& node): m_data(node.m_data), m_next(node.m_next){};
+
+    /*
+     * default destructor for class Node
+     */
     ~Node() = default;
 };
 
@@ -20,32 +31,82 @@ template<class T>
 class Queue
 {
 public:
+    /*
+     * constructor for class Queue(template), starts a new empty queue
+     */
     Queue(): m_size(0), m_head(nullptr), m_tail(nullptr){};
+
+    /*
+     * destructor for class Queue(template), clears all the dynamic allocated memory for the nodes
+     */
     ~Queue();
 
+    /*
+     * copy constructor for class Queue(template), makes a new copy of parameter queue, and allocates to this
+     */
     Queue(const Queue& queue);
 
+    /*
+     * operator= for class Queue(template), makes a new copy of parameter queue, and allocates to this
+     */
     Queue& operator=(const Queue& copy);
 
+    /*
+     * method to add new data to the end of the current(this) queue
+     */
     void pushBack(const T& data);
 
+    /*
+     * returns a reference to the data that was first admitted to the queue
+     */
     T& front();
 
+    /*
+     * returns a const reference to the data that was first admitted to the queue
+     * for the cases of when using const queue
+     */
     const T& front() const;
 
+    /*
+     * deletes the first data that was first admitted to the queue
+     */
     void popFront();
 
+    /*
+     * return the size of the current queue
+     */
     int size()const;
 
+    /*
+     * class for throwing when trying to imply a function on an empty queue
+     */
     class EmptyQueue: public std::exception{};
 
     class Iterator;
     class ConstIterator;
 
+    /*
+     * return an iterator which points to the first data in the queue
+     */
     Iterator begin() ;
+
+    /*
+     * return an iterator which points to the end of the queue
+     * doesn't have any data in this location
+     */
     Iterator end() ;
 
+    /*
+     * return an iterator which points to the first data in the queue
+     * in the case of using const queue
+     */
     ConstIterator begin() const;
+
+    /*
+     * return an iterator which points to the end of the queue
+     * in the case of using const queue
+     * doesn't have any data in this location
+     */
     ConstIterator end() const;
 
 private:
@@ -54,6 +115,10 @@ private:
     Node<T>* m_tail;
 };
 
+/*
+ * filters data in queue that only work in condition
+ * and inserts them to a new Queue
+ */
 template<typename T, typename Condition>
 Queue<T> filter(Queue<T>& queue, const Condition& condition)
 {
@@ -72,6 +137,9 @@ Queue<T> filter(Queue<T>& queue, const Condition& condition)
     return filtered;
 }
 
+/*
+ * transforms the data in the queue by performing the operation that it takes
+ */
 template<typename T, typename Operation>
 void transform(Queue<T>& queue , const Operation& operate)
 {
